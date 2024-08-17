699c36b6fc9c1fd3245aae90c297424e5c70ecd5
thederpykrafter <thederpykrafter@gmail.com>
2024-08-16 21:43:20 -0400

    stylua use Tabs

--- lua/init.lua
+++ lua/init.lua
@@ -1,45 +1,45 @@
 local M = {}
 
 function M.GetScript()
-  local files = {}
-  local tmpfile = "/tmp/stmp.txt"
+	local files = {}
+	local tmpfile = "/tmp/stmp.txt"
 
-  os.execute("ls -1 > " .. tmpfile)
+	os.execute("ls -1 > " .. tmpfile)
 
-  local f = io.open(tmpfile)
-  if not f then
-    return files
-  end
+	local f = io.open(tmpfile)
+	if not f then
+		return files
+	end
 
-  for line in f:lines() do
-    if line == "run" then
-      return "./run"
-    elseif line == "run.sh" then
-      return "./run.sh"
-    elseif line == "Makefile" then
-      return "make test"
-    else
-    end
-  end
+	for line in f:lines() do
+		if line == "run" then
+			return "./run"
+		elseif line == "run.sh" then
+			return "./run.sh"
+		elseif line == "Makefile" then
+			return "make test"
+		else
+		end
+	end
 
-  f:close()
+	f:close()
 end
 
 function M.setup(opts)
-  opts = opts or {}
+	opts = opts or {}
 
-  vim.api.nvim_create_autocmd("TermClose", {
-    callback = function()
-      vim.cmd("close")
-    end,
-  })
+	vim.api.nvim_create_autocmd("TermClose", {
+		callback = function()
+			vim.cmd("close")
+		end,
+	})
 
-  vim.keymap.set("n", "<leader>R", function()
-    if M.GetScript() ~= nil then
-      vim.cmd("vsplit term://" .. M.GetScript())
-      vim.cmd("startinsert")
-    end
-  end, { desc = "Comp-[R]un" })
+	vim.keymap.set("n", "<leader>R", function()
+		if M.GetScript() ~= nil then
+			vim.cmd("vsplit term://" .. M.GetScript())
+			vim.cmd("startinsert")
+		end
+	end, { desc = "Comp-[R]un" })
 end
 
 return M
